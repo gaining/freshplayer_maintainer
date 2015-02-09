16f4931f5090d86af39927abaf66b49e4ebac181
gaining <gaining@users.noreply.github.com>
2015-02-09 18:04:48 -0500

    Update freshplayer_maintainer.sh

--- freshplayer_maintainer.sh
+++ freshplayer_maintainer.sh
@@ -3,7 +3,7 @@
 # Author: Jonathan Soivilus
 # This script facilitates the installation and maintenance of freshplayer plugin on Debian based systems
 # Fresh player wrapper is mainly developed by i-rinat https://github.com/i-rinat/freshplayerplugin 
-# Version 1.1
+# Version 2.0
 # To run this scrip, open the terminal type: chmod +x freshplayer_maintainer.sh; ./freshplayer_maintainer.sh
 # Contact me at: gaining77@outlook.com
 
