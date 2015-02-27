c05b6b6e0e1db0b2ba0d9ca739bd726d21ef94aa
gaining <gaining@users.noreply.github.com>
2015-02-26 22:48:26 -0500

    Added Opera browser support and code cleanup
    Added Opera browser support and did some code cleanup

--- freshplayer_maintainer.sh
+++ freshplayer_maintainer.sh
@@ -3,103 +3,105 @@
 # Author: gaining
 # find out more at  https://github.com/gaining/freshplayer_maintainer
 # This script facilitates the installation and maintenance of freshplayer plugin on Debian based systems
-# Fresh player wrapper is mainly developed by i-rinat https://github.com/i-rinat/freshplayerplugin 
-# Version 2.0
+# Fresh player wrapper is mainly developed by i-rinat https://github.com/i-rinat/freshplayerplugin
+# Version 2.1
 # To run this scrip, open the terminal type: chmod +x freshplayer_maintainer.sh; ./freshplayer_maintainer.sh
 # Contact me at: gaining77@outlook.com
-# file can also be downloaded using my dropbox link with the command: 
-# wget -O freshplayer_maintainer.sh https://db.tt/rHEJ2e0l
+# Updated version of file can also be downloaded using my dropbox link with the command: wget -O freshplayer_maintainer.sh https://db.tt/rHEJ2e0l
+
+FRESHWRAP=~/freshplayerplugin/build/libfreshwrapper-pepperflash.so;
+FRESHCONF=~/.config/freshwrapper.conf;
+FRESHFF=/usr/lib/mozilla/plugins/libfreshwrapper-pepperflash.so;
+FRESHFF2=~/.mozilla/plugins/libfreshwrapper-pepperflash.so;
+FRESHOPERA=/usr/lib/opera/plugins/libfreshwrapper-pepperflash.so;
+PEPFLASH=/opt/google/chrome/PepperFlash/libpepflashplayer.so;
+FFDIR=/usr/lib/mozilla/plugins;
+FFDIR2=~/.mozilla/plugins;
+OPERADIR=/usr/lib/opera/plugins
+
 
 
 function install_plugin(){
   echo "Getting dependencies..."
-  HWFILE=/opt/google/chrome/PepperFlash/libpepflashplayer.so;
-    
-  if [ -f $HWFILE ];then
-     sudo apt-get install build-essential git cmake pkg-config libglib2.0-dev libasound2-dev libx11-dev libgl1-mesa-dev libgles2-mesa-dev liburiparser-dev libcairo2-dev libpango1.0-dev libpangocairo-1.0-0 libpangoft2-1.0-0 libfreetype6-dev libgtk2.0-dev libxinerama-dev libconfig-dev libevent-dev libssl-dev;
-  else 
-     sudo add-apt-repository -y ppa:skunk/pepper-flash;
-     sudo apt-get update;
-     sudo apt-get install --no-install-recommends pepflashplugin-installer ;
-     sudo apt-get install build-essential git cmake pkg-config libglib2.0-dev libasound2-dev libx11-dev libgl1-mesa-dev libgles2-mesa-dev liburiparser-dev libssl-dev libcairo2-dev libpango1.0-dev libpangocairo-1.0-0 libpangoft2-1.0-0 libfreetype6-dev libgtk2.0-dev libxinerama-dev libconfig-dev libevent-dev ragel;    
-     sudo mkdir -p /opt/google/chrome/PepperFlash; 
-     sudo ln -s /usr/lib/pepflashplugin-installer/libpepflashplayer.so /opt/google/chrome/PepperFlash;
+  if [ -f $PEPFLASH ];then
+    sudo apt-get install build-essential git cmake pkg-config libglib2.0-dev libasound2-dev libx11-dev libgl1-mesa-dev libgles2-mesa-dev liburiparser-dev libcairo2-dev libpango1.0-dev libpangocairo-1.0-0 libpangoft2-1.0-0 libfreetype6-dev libgtk2.0-dev libxinerama-dev libconfig-dev libevent-dev libssl-dev;
+  else
+    sudo add-apt-repository -y ppa:skunk/pepper-flash;
+    sudo apt-get update;
+    sudo apt-get install --no-install-recommends pepflashplugin-installer ;
+    sudo apt-get install build-essential git cmake pkg-config libglib2.0-dev libasound2-dev libx11-dev libgl1-mesa-dev libgles2-mesa-dev liburiparser-dev libssl-dev libcairo2-dev libpango1.0-dev libpangocairo-1.0-0 libpangoft2-1.0-0 libfreetype6-dev libgtk2.0-dev libxinerama-dev libconfig-dev libevent-dev ragel;
+    sudo mkdir -p /opt/google/chrome/PepperFlash;
+    sudo ln -s /usr/lib/pepflashplugin-installer/libpepflashplayer.so /opt/google/chrome/PepperFlash;
   fi
   
   cd;
- 
+  
   git clone https://github.com/i-rinat/freshplayerplugin.git;
   
-  cd freshplayerplugin; mkdir build; cd build; cmake ..; make; 
+  cd freshplayerplugin; mkdir build; cd build; cmake ..; make;
   
   echo ""
-
+  
   while true
   do
-  read -p "Install plugin system wide? [Y/N] " yn
-
+    read -p "Install plugin system wide? [Y/N] " yn
+    
     case "$yn" in
-            y|Y)  sudo ln -s ~/freshplayerplugin/build/libfreshwrapper-pepperflash.so /usr/lib/mozilla/plugins
-		break;;
-            n|N)  mkdir -p ~/.mozilla/plugins; ln -s ~/freshplayerplugin/build/llibfreshwrapper-pepperflash.so ~/.mozilla/plugins
-		break;;
-             * ) echo  -e $RED "Invalid Choice";;
+      y|Y)  sudo ln -fs $FRESHWRAP $FFDIR;
+        sudo ln -fs $FRESHWRAP $FFDIR;
+      break;;
+      n|N)  mkdir -p $FFDIR2;
+        ln -s $FRESHWRAP $FFDIR2;
+      break;;
+      * ) echo  -e "$RED" "Invalid Choice";;
     esac
   done
-
+  
   echo ""
   echo "Installation complete!"
   echo ""
-
+  
 }
 
 function uninstall_plugin(){
   rm -rf ~/freshplayerplugin;
-  HWFILE=~/.config/freshwrapper.conf;
-  FRESHFILE=/usr/lib/mozilla/plugins/libfreshwrapper-pepperflash.so;
-  FRESHFILE2=~/.mozilla/plugins/libfreshwrapper-pepperflash.so;
-  sudo rm -f $HWFILE $FRESHFILE $FRESHFILE2;
+  sudo rm -f $FRESHCONF $FRESHFF $FRESHFF2 $FRESHOPERA;
 }
 
 function update_plugin(){
   cd ~/freshplayerplugin; git pull; cd build; cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..; make;
 }
-function enable_disable_hW(){
-  HWFILE=~/.config/freshwrapper.conf;
-    
-  if [ -f $HWFILE ];
+
+function switch_HWA(){
+  if [ -f $FRESHCONF ];
   then
-     rm  $HWFILE;
+    rm  $FRESHCONF;
     printf "Hardware acceleration DISABLED \n\n"
-  else 
-    echo enable_3d = 1 > $HWFILE;
-    echo "flash_command_line = \"enable_hw_video_decode=1,enable_stagevideo_auto=1\"" >>$HWFILE
+  else
+    echo enable_3d = 1 > $FRESHCONF;
+    echo "flash_command_line = \"enable_hw_video_decode=1,enable_stagevideo_auto=1\"" >>$FRESHCONF
     printf "Hardware acceleration ENABLED \n\n"
     
   fi
 }
 
 
-function enable_disable(){
-  FILELOC1=~/freshplayerplugin/build/libfreshwrapper-pepperflash.so;
-  FILELOC2=/usr/lib/mozilla/plugins/libfreshwrapper-pepperflash.so;
-  FILELOC3=~/.mozilla/plugins/libfreshwrapper-pepperflash.so;
+function switch_plugin(){
   
-
-   if [[ -f $FILELOC2 || -f $FILELOC3 ]]; then
-    sudo rm -f $FILELOC2 $FILELOC3;
+  if [[ -f $FRESHFF || -f $FRESHFF2 || -f $FRESHOPERA ]]; then
+    sudo rm -f $FRESHFF $FRESHFF2 $FRESHOPERA;
     printf "fresh player plugin DISABLED \n\n"
-    
-  else 
-  while true
-   do
-     read -p "Enable system wide or not [Y/N]: " yn
-    case "$yn" in
-            y|Y)sudo ln -s $FILELOC1 /usr/lib/mozilla/plugins 
-		break;;
-            n|N)  ln -s $FILELOC1 ~/.mozilla/plugins
-		break;;
-             * ) echo  -e $RED "Invalid choice";;
+  else
+    while true
+    do
+      read -p "Enable system wide? (includes Opera browser)[Y/N]: " yn
+      case "$yn" in
+        y|Y)sudo ln -sf $FRESHWRAP $FFDIR;
+          sudo ln -sf $FRESHWRAP $OPERADIR;
+        break;;
+        n|N)  ln -s $FRESHWRAP $FFDIR2
+        break;;
+        * ) echo  -e "$RED" "Invalid choice";;
       esac
     done
     printf "fresh player plugin ENABLED \n\n"
@@ -111,7 +113,7 @@ k=1
 
 clear
 
-printf "Welcome to fresh player plugin Maintainer v2.0 \n\n"
+printf "Welcome to fresh player plugin Maintainer v2.1 \n\n"
 
 PS3='Choose an option: '
 
@@ -125,10 +127,10 @@ do
       install_plugin;
     ;;
     "enable or disable hardware acceleration")
-      enable_disable_hW
+      switch_HWA
     ;;
     "enable or disable plugin")
-      enable_disable
+      switch_plugin
     ;;
     "update plugin")
       echo "Updating fresh player plugin..."
@@ -136,16 +138,16 @@ do
       echo ""
     ;;
     "uninstall plugin")
-     echo "uninstalling plugin..."
+      echo "uninstalling plugin..."
       uninstall_plugin
-     echo ""
-     echo "fresh player plugin has been successfully removed from your system"
-     echo ""
-     ;;
+      echo ""
+      echo "fresh player plugin has been successfully removed from your system"
+      echo ""
+    ;;
     "Quit")
       break
     ;;
-    *) echo  -e $RED "invalid option";;
+    *) echo  -e "$RED" "invalid option";;
   esac
   
   for i in "${options[@]}"
@@ -157,6 +159,3 @@ do
   
 done
 clear
-
-
-
